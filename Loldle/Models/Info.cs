651666de8253f302bc3a355d1d81c258c6edf907
PotatoW7 <anonymoushex6@gmail.com>
2025-02-14 12:06:43 +0200

    commit1

---
+++ Loldle/Models/Info.cs
@@ -0,0 +1,12 @@
+﻿namespace Loldle.Models
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public List<string> Name { get; set; }
+        public List<string> Gender { get; set; }
+        public List<string> Type { get; set; }
+        public List<string> Race { get; set; }
+        public List<string> Region { get; set; }
+    }
+}
